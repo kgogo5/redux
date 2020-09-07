bad65f7fe68f2b3320b070d798210ee5fbdb9887
https://github.com/kgogo5/ <kgogo9@gmail.com>
2020-09-08 02:44:32 +0900

    이미지 파일 업로드 작업

--- src/Components/Editor.js
+++ src/Components/Editor.js
@@ -2,12 +2,34 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { Editor, EditorState, RichUtils } from "draft-js";
 import "draft-js/dist/Draft.css";
+import Axios from "axios";
 
 function MyEditor() {
   const [editorState, setEditorState] = useState(() =>
     EditorState.createEmpty()
   );
 
+  const [filesState, setFiles] = useState(null);
+
+  // const FileList = () => {
+  //   console.log(filesState);
+  //   return (
+  //     <ul>
+  //       {filesState !== null
+  //         ? filesState.map((e) => {
+  //             return (
+  //               <li>
+  //                 <span>{e.name}</span>
+  //                 <span>{e.lastModified}</span>
+  //               </li>
+  //             );
+  //           })
+  //         : null}
+  //     </ul>
+  //   );
+  // };
+
+  // 키보드 단축키 기능
   function handleKeyCommand(command, editorState) {
     const newState = RichUtils.handleKeyCommand(editorState, command);
     if (newState) {
@@ -17,9 +39,22 @@ function MyEditor() {
     return "not-handled";
   }
 
+  // 버튼 클릭시 볼드
   function _onBoldClick() {
     setEditorState(RichUtils.toggleInlineStyle(editorState, "BOLD"));
   }
+
+  function fileUpload(e) {
+    setFiles({
+      filesState: e.target.files[0],
+    });
+  }
+
+  const fileSelectedHandler = () => {
+    const filesData = new FormData();
+    filesData.append("image", filesState.name, filesState.lastModified);
+    console.log(filesState);
+  };
 
   return (
     <>
@@ -29,6 +64,11 @@ function MyEditor() {
         handleKeyCommand={handleKeyCommand}
         onChange={setEditorState}
       />
+      <form method="post" encType="multipart/form-data">
+        <input type="file" onChange={fileUpload} />
+        <input type="button" value="추가하기" onClick={fileSelectedHandler} />
+        {/* <FileList /> */}
+      </form>
     </>
   );
 }
